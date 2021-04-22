08dcdf44b66d659e2b0685c7a78a71652c9587cf
riyajaggi <jaggiriya20@gmail.com>
2021-04-22 02:29:03 -0400

    Fix 500 error

--- app/__init__.py
+++ app/__init__.py
@@ -30,3 +30,7 @@ socketio.init_app(app)
 @app.errorhandler(404)
 def not_found(error):
   return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def not_found(error):
+  return render_template("500.html"), 500
