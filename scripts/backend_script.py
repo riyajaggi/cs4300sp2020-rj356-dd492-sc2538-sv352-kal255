7e4e029585c823f4e597b8b7cee9883b3b8f80b2
Kendall Lane <kal255@cornell.edu>
2021-04-30 18:24:47 -0400

    Create Final Search Function and Basic Implementation

--- scripts/backend_script.py
+++ scripts/backend_script.py
@@ -2,6 +2,8 @@ import re
 import os
 import numpy as np
 import json
+import review_similarity
+import adhoc_similarity
 
 def jaccardRanking(show, N=3):
     """
@@ -29,3 +31,64 @@ def jaccardRanking(show, N=3):
 
 # jaccardRanking ("Friends", 3)
 # print(jaccardRanking("Criminal Minds"))
+def final_search(query_show, n,  free_search=None, genre=None, ):
+    """
+    """
+
+    weights = {
+        'transcripts' : .20 ,
+        'reviews' : .80,
+        'descriptions' : 0,
+        'genre' : 0,
+        'free search' : 0,
+    }
+
+    results = []
+    tv_sim_score_sum = {}
+    transcripts_ranking = jaccardRanking(query_show, n) # list of tv shows
+    reviews_ranking = review_similarity.find_n_similar_shows_reviews(query_show, n*2) # list of tv shows and sim scores
+    # desc_ranking = 
+    free_search_ranking = []
+    if free_search is not None:
+        free_search_ranking = adhoc_similarity.find_n_similar_shows_free_search(free_search, n*2) # list of tv shows and sim scores
+        weights['transcripts'] = .15
+        weights['reviews'] = .50
+        # weights['descriptions'] = .45
+        weights['free search'] = .35
+        for show, score in free_search_ranking:
+            if show in tv_sim_score_sum:
+                tv_sim_score_sum[show] += weights['free search'] * score * 100
+            else:
+                tv_sim_score_sum[show] = weights['free search'] * score * 100
+    genre_search_ranking = []
+    # if genre is not None:
+    #     # genre_ranking =   
+    
+    for i in range(len(transcripts_ranking)):
+        show = transcripts_ranking[i]
+        score = (.5 - ((i+1)/100))
+        if show in tv_sim_score_sum:
+            tv_sim_score_sum[show] += weights['transcripts'] * score * 100
+        else:
+            tv_sim_score_sum[show] = weights['transcripts'] * score * 100
+
+    for show, score in reviews_ranking:
+        if show in tv_sim_score_sum:
+            tv_sim_score_sum[show] += weights['reviews'] * score * 100
+        else:
+            tv_sim_score_sum[show] = weights['reviews'] * score * 100
+    
+    tv_sim_score_sum = {k: v for k, v in sorted(tv_sim_score_sum.items(), key=lambda item: item[1])}
+    index = 0
+    for key, _ in tv_sim_score_sum.items():
+        results.append(key)
+        index += 1
+        if index == n:
+            break
+    return results
+
+the_walking_dead_results = final_search("The Walking Dead", 10)
+print(the_walking_dead_results)
+
+
+
