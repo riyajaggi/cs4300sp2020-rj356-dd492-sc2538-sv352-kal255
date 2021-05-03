bdfef8d9aa8eef40718d9aec9e9aa53da2b14932
riyajaggi <jaggiriya20@gmail.com>
2021-05-02 20:01:06 -0400

    remove print statements

--- scripts/backend_script.py
+++ scripts/backend_script.py
@@ -170,8 +170,8 @@ def final_search(query_show, n, free_search=None, genre=None):
     return results
 
 # TESTS
-the_walking_dead_results = final_search("The Walking Dead", 10)
-print(the_walking_dead_results)
+# the_walking_dead_results = final_search("The Walking Dead", 10)
+# print(the_walking_dead_results)
 # sherlock_results = final_search("Sherlock", 10, "dogs")
 # print(sherlock_results)
 # its_always_sunny_results = final_search("It's Always Sunny in Philadephia", 10) # no reviews, no description, no transcripts in transcript2
